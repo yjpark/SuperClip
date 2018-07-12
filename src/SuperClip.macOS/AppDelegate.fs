f8c5e2f5f3d7090d58fa6e49150be6e6988765b7
YJ Park <yjpark@gmail.com>
2018-07-12 23:07:40 +0800

    refs #1, add macOS project, currently not use paket yet

---
+++ src/SuperClip.macOS/AppDelegate.fs
@@ -0,0 +1,26 @@
+﻿namespace SuperClip.macOS
+open System
+open Foundation
+open AppKit
+
+open Xamarin.Forms
+open Xamarin.Forms.Platform.MacOS
+
+module AppTypes = SuperClip.App.Types
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit FormsApplicationDelegate ()
+
+    let style = NSWindowStyle.Closable ||| NSWindowStyle.Resizable ||| NSWindowStyle.Titled
+    let rect = new CoreGraphics.CGRect (200.0, 100.0, 1024.0, 768.0)
+    let window = new NSWindow (rect, style, NSBackingStore.Buffered, false)
+    do (
+        window.Title <- "Xamarin.Forms on Mac!"
+        //window.TitleVisibility <- NSWindowTitleVisibility.Hidden
+    )
+    override this.MainWindow = window
+    override this.DidFinishLaunching (notification : NSNotification) =
+        Forms.Init ()
+        this.LoadApplication (new AppTypes.App())
+        base.DidFinishLaunching(notification)
---
+++ src/SuperClip.macOS/Main.fs
@@ -0,0 +1,11 @@
+﻿namespace SuperClip.macOS
+open System
+open AppKit
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        NSApplication.Init ()
+        NSApplication.SharedApplication.Delegate <- new AppDelegate();
+        NSApplication.Main (args)
+        0
