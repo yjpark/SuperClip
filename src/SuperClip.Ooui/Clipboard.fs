c6b03f3a052e94c7f06267e089dbcd03f7b77fa8
YJ Park <yjpark@gmail.com>
2018-08-02 17:46:38 +0800

    fixes , use shell to access clipboard

--- src/SuperClip.Ooui/Clipboard.fs
+++ src/SuperClip.Ooui/Clipboard.fs
@@ -8,13 +8,29 @@ open NodaTime
 open Plugin.Clipboard.Abstractions;
 
 open Dap.Platform
+open Dap.Local
 
 type ClipboardImplementation () =
     let mutable text' = ""
     member _this.GetText () =
-        text'
+        match Runtime.Platform with
+        | Mac ->
+            Shell.bash "pbpaste"
+        | Linux ->
+            Shell.bash "xsel -b"
+        | Windows ->
+            text'
     member _this.SetText text =
         text' <- text
+        let text = text |> Shell.escape
+        match Runtime.Platform with
+        | Mac ->
+            Shell.bash <| sprintf "echo \"%s\" | pbcopy" text
+        | Linux ->
+            Shell.bash <| sprintf "echo \"%s\" | xclip -selection clipboard" text
+        | Windows ->
+            Shell.bat <| sprintf "echo %s | clip" text
+        |> ignore
     interface IClipboard with
         member this.GetTextAsync () = task {
             return this.GetText ()
