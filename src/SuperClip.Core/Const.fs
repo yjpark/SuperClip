39f183551387aff90665032305d1dc678a14f7f0
YJ Park <yjpark@gmail.com>
2018-08-18 13:51:12 +0800

    use cloud server

--- src/SuperClip.Core/Const.fs
+++ src/SuperClip.Core/Const.fs
@@ -11,7 +11,8 @@ let PassHashSalt = "ahDaefam2DecaR0ooyak6ohghah0coh6"
 let CryptoKeySalt = "shoo4ree3daiH9go4Yeir8rah4ua7fei"
 
 [<Literal>]
-let CloudServerUri = "ws://localhost:5700/ws_user"
+//let CloudServerUri = "ws://localhost:5700/ws_user"
+let CloudServerUri = "ws://yjpark.org:5700/ws_user"
 
 let calcPassHash (password : string) =
     calcSha256SumWithSalt PassHashSalt password
