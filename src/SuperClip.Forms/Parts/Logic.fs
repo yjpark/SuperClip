7c3a4d9b949227ba2bf909220ef98f8aa4f663e0
YJ Park <yjpark@gmail.com>
2018-08-22 12:56:09 +0800

    follow lib changes

--- src/SuperClip.Forms/Parts/Logic.fs
+++ src/SuperClip.Forms/Parts/Logic.fs
@@ -29,7 +29,7 @@ let initAsync (app : Simple.Model) = task {
     let! history = app.Env |> Helper.doSetupAsync
     let primary = app.Env |> PrimaryService.get NoKey
     do! app.Env |> PacketClient.registerAsync true None
-    let! cloudStub = app.Env |> WebSocketProxy.addAsync NoKey CloudTypes.StubSpec CloudServerUri true
+    let! cloudStub = app.Env |> WebSocketProxy.addAsync NoKey CloudTypes.StubSpec CloudServerUri (Some 5.0<second>) true
     let args : SessionService.Args =
         {
             Stub = cloudStub
