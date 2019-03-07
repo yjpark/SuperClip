ba9f8e958ec1fd4e3ae4bae28008aeb9b71d6ea1
YJ Park <yjpark@gmail.com>
2019-03-07 13:27:42 +0800

    filter out self from client side channel service

--- src/SuperClip.App/Session/Tasks.fs
+++ src/SuperClip.App/Session/Tasks.fs
@@ -13,39 +13,49 @@ open SuperClip.App.Session.Types
 module ChannelTypes = SuperClip.Core.Channel.Types
 module ChannelService = SuperClip.Core.Channel.Service
 
+let private isNotSelf (runner : Agent) (device : Device) =
+    match runner.Actor.State.Auth with
+    | Some auth -> auth.Device.Guid <> device.Guid
+    | None -> true
+
 let doSetChannelAsync (peers : Peers) : GetTask<Agent, unit> =
     fun runner -> task {
         let! channel = runner |> setupChannelServiceAsync peers.Channel
         runner.Deliver <| InternalEvt ^<| SetChannel (peers, channel)
-        channel.Post <| ChannelTypes.DoSetDevices peers.Devices None
+        let devices =
+            peers.Devices
+            |> List.filter ^<| isNotSelf runner
+        channel.Post <| ChannelTypes.DoSetDevices devices None
     }
 
 let doAddToChannelAsync (peer : Peer) : GetTask<Agent, unit> =
     fun runner -> task {
-        match runner.Actor.State.Channel with
-        | Some channel ->
-            if channel.Ident.Key = peer.Channel.Key then
-                let! ok = channel.PostAsync <| ChannelTypes.DoAddDevice peer.Device
-                if ok then
-                    runner.Deliver <| Evt ^<| OnDevicesChanged channel.Actor.State.Devices
-            else
-                logError runner "doAddToChannelAsync" "Wrong_Channel" (channel.Ident.Key, peer)
-        | None ->
-            logError runner "doAddToChannelAsync" "No_Channel" peer
-            ()
+        if isNotSelf runner peer.Device then
+            match runner.Actor.State.Channel with
+            | Some channel ->
+                if channel.Ident.Key = peer.Channel.Key then
+                    let! ok = channel.PostAsync <| ChannelTypes.DoAddDevice peer.Device
+                    if ok then
+                        runner.Deliver <| Evt ^<| OnDevicesChanged channel.Actor.State.Devices
+                else
+                    logError runner "doAddToChannelAsync" "Wrong_Channel" (channel.Ident.Key, peer)
+            | None ->
+                logError runner "doAddToChannelAsync" "No_Channel" peer
+                ()
     }
 
 let doRemoveFromChannelAsync (peer : Peer) : GetTask<Agent, unit> =
     fun runner -> task {
-        match runner.Actor.State.Channel with
-        | Some channel ->
-            if channel.Ident.Key = peer.Channel.Key then
-                let! ok = channel.PostAsync <| ChannelTypes.DoRemoveDevice peer.Device
-                if ok then
-                    runner.Deliver <| Evt ^<| OnDevicesChanged channel.Actor.State.Devices
-            else
-                logError runner "doRemoveFromChannelAsync" "Wrong_Channel" (channel.Ident.Key, peer)
-        | None ->
-            logError runner "doRemoveFromChannelAsync" "No_Channel" peer
-            ()
+        if isNotSelf runner peer.Device then
+            match runner.Actor.State.Channel with
+            | Some channel ->
+                if channel.Ident.Key = peer.Channel.Key then
+                    let! ok = channel.PostAsync <| ChannelTypes.DoRemoveDevice peer.Device
+                    if ok then
+                        runner.Deliver <| Evt ^<| OnDevicesChanged channel.Actor.State.Devices
+                else
+                    logError runner "doRemoveFromChannelAsync" "Wrong_Channel" (channel.Ident.Key, peer)
+            | None ->
+                logError runner "doRemoveFromChannelAsync" "No_Channel" peer
+                ()
     }
