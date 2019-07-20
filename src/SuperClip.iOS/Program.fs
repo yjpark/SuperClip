6a191bc5a6d1b1f8e10bee173cc80435aa9a89f0
YJ Park <yjpark@gmail.com>
2019-07-20 15:26:22 +0800

    comment out partial logic

--- src/SuperClip.iOS/Program.fs
+++ src/SuperClip.iOS/Program.fs
@@ -7,9 +7,24 @@ open Dap.Context
 open Dap.Platform
 open Dap.iOS
 open Dap.Fabulous.iOS
+open Dap.Gui
 
 open SuperClip.App
 open SuperClip.Fabulous
+
+type IOSThemeHook (logging : ILogging) =
+    inherit EmptyContext(logging, "AndroidThemeHook")
+    interface IGuiAppHook with
+        member this.OnInit (app : IGuiApp) =
+            ()
+            (*
+            app.OnWillSwitchTheme.AddWatcher this "OnWillSwitchTheme" (fun theme ->
+                if theme.Key = Theme.DarkTheme then
+                    MainActivity.Instance.SwitchDarkTheme ()
+                else
+                    MainActivity.Instance.SwitchLightTheme ()
+            )
+            *)
 
 [<EntryPoint>]
 [<STAThread>]
@@ -17,13 +32,4 @@ let main args =
     setFabulousIOSParam <| IOSParam.Create ("SuperClip.iOS")
     App.RunFabulous ("super-clip-.log")
 
-type IOSThemeHook (logging : ILogging) =
-    inherit EmptyContext(logging, "AndroidThemeHook")
-    interface IGuiAppHook with
-        member this.OnInit (app : IGuiApp) =
-            app.OnWillSwitchTheme.AddWatcher this "OnWillSwitchTheme" (fun theme ->
-                if theme.Key = Theme.DarkTheme then
-                    MainActivity.Instance.SwitchDarkTheme ()
-                else
-                    MainActivity.Instance.SwitchLightTheme ()
-            )
+
