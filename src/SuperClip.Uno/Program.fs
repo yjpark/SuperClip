89682084ab14e213d416c123a6d0d9f533f8171a
YJ Park <yjpark@gmail.com>
2020-01-03 23:19:36 +0800

    wip

---
+++ src/SuperClip.Uno/Program.fs
@@ -0,0 +1,18 @@
+﻿module SuperClip.Uno.Program
+
+open System
+
+open Dap.Prelude
+open Dap.Context
+open Dap.Platform
+open Dap.Fabulous.Uno
+
+open SuperClip.App
+open SuperClip.Fabulous
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv =
+    setFabulousUnoParam <| UnoParam.Create ("SuperClip.Uno", port = 6060)
+    App.RunFabulous ("super-clip-.log")
+
